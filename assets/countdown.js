537414f0af0dae4b6d09186f39a701c32b0a1356
fredeil <fredrik.eilertsen@gmail.com>
2018-08-26 21:57:34 +0200

    Update countdown.js

--- assets/countdown.js
+++ assets/countdown.js
@@ -1,53 +1,32 @@
-const countDownClock = (number = 100, format = 'seconds') => 
-{
-    const d = document;
-    const daysElement = d.querySelector('.days');
-    const hoursElement = d.querySelector('.hours');
-    const minutesElement = d.querySelector('.minutes');
-    const secondsElement = d.querySelector('.seconds');
+const countDownClock = (eventDate) => {
+  let d = document;
+  let daysElement = d.querySelector('.days');
+  let hoursElement = d.querySelector('.hours');
+  let minutesElement = d.querySelector('.minutes');
+  let secondsElement = d.querySelector('.seconds');
+  let seconds = Math.abs(Date.now() - eventDate)
 
-    let countdown;
+  return timer(seconds);
 
-    convertFormat(format);
-    
-    function convertFormat(format) 
-    {
-      switch(format) {
-        case 'seconds':
-          return timer(number);
-        case 'minutes':
-          return timer(number * 60);
-          case 'hours':
-          return timer(number * 60 * 60);
-        case 'days':
-          return timer(number * 60 * 60 * 24);             
-      }
-    }
+  function timer(seconds) {
+    let now = Date.now();
+    let then = now + seconds;
 
-    function timer(seconds) 
-    {
-      const now = Date.now();
-      const then = now + seconds * 1000;
-  
-      countdown = setInterval(() => {
-        const secondsLeft = Math.round((then - Date.now()) / 1000);
-  
-        if(secondsLeft <= 0) 
-        {
-          clearInterval(countdown);
-          return;
-        };
-  
-        displayTimeLeft(secondsLeft);
-  
-      }, 1000);
-    }
-  
-    function displayTimeLeft(seconds) 
-    {
-      daysElement.textContent = Math.floor(seconds / 86400);
-      hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
-      minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
-      secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
-    }
+    let countdown = setInterval(() => {
+      const secondsLeft = Math.round((then - Date.now()) / 1000);
+      if (secondsLeft <= 0) {
+        clearInterval(countdown);
+        secondsElement.textContent = 0;
+        return;
+      };
+      displayTimeLeft(secondsLeft);
+    }, 1000);
+  }
+
+  function displayTimeLeft(seconds) {
+    daysElement.textContent = Math.floor(seconds / 86400);
+    hoursElement.textContent = Math.floor((seconds % 86400) / 3600);
+    minutesElement.textContent = Math.floor((seconds % 86400) % 3600 / 60);
+    secondsElement.textContent = seconds % 60 < 10 ? `0${seconds % 60}` : seconds % 60;
+  }
 }
